26ac8b2139a462196bf7ac87fe797dfb9fb34c1c
Rimsky <Rimsky@DESKTOP-BO2IQFC.lan>
2023-04-09 22:03:04 -0400

    "Homework One"

---
+++ src/packageOne/ClassOneHW.java
@@ -0,0 +1,13 @@
+package packageOne;
+
+public class ClassOneHW {
+	
+	public static void main (String args []) {
+		System.out.println("This is going to become super easy");
+	}
+	
+	public void addMethod (int a, int b) {
+		System.out.println(a + b);
+	}
+
+}
